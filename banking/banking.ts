2b63f012111c61ab47c45dc59f6bbffe61eeb96a
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2025-10-28 15:33:57 +0100

    fix: get rid of cc todo workflow

--- banking/banking.ts
+++ banking/banking.ts
@@ -5,31 +5,10 @@ import {
   WithdrawalError,
 } from "./types";
 
-function generateTransactionId(): string {
-  return `txn_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
-}
-
-export function createAccount(account: BankAccount): BankAccount | WithdrawalError {
-  if (account.balance < 0) {
-    return {
-      code: "INVALID_AMOUNT",
-      message: "Account balance cannot be negative"
-    };
-  }
-
-  if (account.balance === 0) {
-    return {
-      code: "INVALID_AMOUNT",
-      message: "Initial account balance must be positive"
-    };
-  }
-
-  return account;
-}
+export function createAccount(account: BankAccount): BankAccount | WithdrawalError {}
 
 export function processWithdrawal(
   account: BankAccount,
   withdrawal: WithdrawalRequest
 ): WithdrawalResult | WithdrawalError {
-  // TODO(human): Implement withdrawal processing logic with validation
 }
